78b1992a143fc3b693d11c6c42fef824bede76f9
serensiahaan <serenelizabethsiahaan@gmail.com>
2024-11-23 20:16:31 +0700

    fix: tambah metode adt pembacaan stdin, wordtoint, dan compare

--- src/start.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-#include "mesinkarakter.h"
-#include "mesinkata.h"
-#include "array.h"
-#include "arraydin.h"
-
-void startStore(ArrayDin * arrayItems, TabInt* arrayUsers);
