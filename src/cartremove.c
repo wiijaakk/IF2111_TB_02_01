e447ff901701256926bda1550e2796b62e473714
18223036-Favian <148781301+19623238-Favian@users.noreply.github.com>
2024-12-17 15:40:58 +0700

    Fix: cartremove function name fix

--- src/cartremove.c
+++ src/cartremove.c
@@ -1,11 +1,11 @@
 #include "cartremove.h"
 
 void cartremove(Map cart, Barang barang, int quantity) {
-    if (!IsMember(cart, &barang)) {
+    if (!IsMemberMap(cart, &barang)) {
         printf("Barang tidak ada di keranjang belanja!\n");
     } else {
         if (quantity > ValueMap(cart, &barang)) {
-            printf("Tidak berhasil mengurangi, hanya terdapat %d %s pada keranjang!\n", Value(cart,&barang), barang);
+            printf("Tidak berhasil mengurangi, hanya terdapat %d %s pada keranjang!\n", ValueMap(cart,&barang), barang);
         } else {
             DeleteMap(&cart, &barang, quantity);
             printf("Berhasil mengurangi %d %s dari keranjang belanja!\n", quantity, barang);
