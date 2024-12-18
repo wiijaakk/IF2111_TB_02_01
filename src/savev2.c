d3907bc3c3fbb0caf4553d5d1f6fb7592d25183e
serensiahaan <serenelizabethsiahaan@gmail.com>
2024-12-18 23:26:23 +0700

    fix: save2.c untuk urutan barang di riwayat_pembelian

--- src/savev2.c
+++ src/savev2.c
@@ -2,6 +2,8 @@
 
 void save(char* fileName, ArrayDin* arrayItems, TabInt* arrayUsers) { // Fungsi untuk command SAVE
     char fullPath[200];
+    Stack tempStack;
+    CreateEmptyStack(&tempStack);
 
     snprintf(fullPath, sizeof(fullPath), "%s%s", SAVE_FOLDER, fileName); // Menggabungkan folder (../save/) dengan filename input user
 
@@ -19,27 +21,38 @@ void save(char* fileName, ArrayDin* arrayItems, TabInt* arrayUsers) { // Fungsi 
         fprintf(file, "%d\n", NbElmt(*arrayUsers)); // Memasukkan baris Total Users
         for(int i = 0; i < NbElmt(*arrayUsers); i++){
             fprintf(file, "%d %s %s\n", arrayUsers->TI[i].money, arrayUsers->TI[i].name, arrayUsers->TI[i].password);
-            // Perlu pakai size agar bisa hitung jumlah elemen tanpa pop
-            // karena save harus bisa digunakan tanpa quit
+
+            // Perlu atribut size untuk hitung jumlah elemen
             int total = (GetElmt(*arrayUsers, i)).riwayat_pembelian->size;
             fprintf(file, "%d\n", total);
             
+            // Memasukkan riwayat_pembelian user ke tempStack dahulu
+            // untuk menjaga urutan penulisan barang_dibeli yang terbalik
             barang_dibeli currentBarangDibeli;
             while(!IsEmptyStack(*arrayUsers->TI[i].riwayat_pembelian)){
                 PopStack(arrayUsers->TI[i].riwayat_pembelian, &currentBarangDibeli);
-                printf("%d %s\n", currentBarangDibeli.totalharga, currentBarangDibeli.name);
-                fprintf(file, "%d %s\n", currentBarangDibeli.totalharga, currentBarangDibeli.name);
+                PushStack(&tempStack, currentBarangDibeli);
             }
 
+            // Memasukkan barang_dibeli ke file
+            // dan memasukkannya kembali ke stack riwayat_pembelian
+            // jika user tidak quit setelah save
+            while(!IsEmptyStack(tempStack)){
+                PopStack(&tempStack, &currentBarangDibeli);
+                fprintf(file, "%d %s\n", currentBarangDibeli.totalharga, currentBarangDibeli.name);
+                PushStack(arrayUsers->TI[i].riwayat_pembelian, currentBarangDibeli);
+            }
+
+            // Menghitung jumlah barang wishlist dan memasukkannya ke file
             total = 0;
             address currentAddress = First(*(arrayUsers->TI[i].wishlist));
             while(currentAddress != Nuh){
                 total++;
                 currentAddress = currentAddress->next;
             }
-
             fprintf(file, "%d\n", total);
 
+            // Memasukkan baris nama barang di wishlist
             currentAddress = First(*(arrayUsers->TI[i].wishlist));
             for(int i = 0; i < total; i++){
                 fprintf(file, "%s\n", Info(currentAddress));
