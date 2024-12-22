874811c3c6ab4aba55032c933d13bf06df4e96d2
serensiahaan <serenelizabethsiahaan@gmail.com>
2024-12-22 14:13:47 +0700

    fix: more load.c check condition

--- src/load.c
+++ src/load.c
@@ -4,10 +4,15 @@
 
 void load(Word filename, ArrayDin* arrayItems, TabInt* arrayUsers, boolean * SessionStatus) {
     int total = 0;
-    int n_riwayat_pembelian, n_wishlist;
+    int n_riwayat_pembelian = -1;
+    int n_wishlist = -1;
     Barang currentBarang;
     User currentUser;
     boolean exists;
+    boolean isIntBarang = true;
+    boolean isIntUser = true;
+    boolean riwayat_exist = true;
+    boolean wishlist_exist = true;
 
     char filenameStr[50];
     char fullPath[200];
@@ -21,87 +26,134 @@ void load(Word filename, ArrayDin* arrayItems, TabInt* arrayUsers, boolean * Ses
         // Membuka file
         StartFileWord(fullPath);
         if (!EndWord) {
-            // Membaca total barang yang tersimpan dalam file
-            total = wordToInt(currentWord);
 
-            // Memasukkan informasi barang ke arrayItems
-            for (int i = 0; i < total; i++) {
-                ADVFileWordSpace();
-                currentBarang.price = wordToInt(currentWord);
-
-                ADVFileWordNewLine();
-                for (int j = 0; j < currentWord.Length; j++) {
-                    currentBarang.name[j] = currentWord.TabWord[j];
+            for(int i = 0; i < currentWord.Length; i++){
+                if(currentWord.TabWord[i] < 48 || currentWord.TabWord[i] > 57){
+                    isIntBarang = false;
                 }
-                currentBarang.name[currentWord.Length] = '\0';
+            }
+            
+            if(isIntBarang){
+                // Membaca total barang yang tersimpan dalam file
+                total = wordToInt(currentWord);
 
-                InsertLastArrDin(arrayItems, currentBarang);
+                // Memasukkan informasi barang ke arrayItems
+                for (int i = 0; i < total; i++) {
+                    ADVFileWordSpace();
+                    currentBarang.price = wordToInt(currentWord);
+
+                    ADVFileWordNewLine();
+                    for (int j = 0; j < currentWord.Length; j++) {
+                        currentBarang.name[j] = currentWord.TabWord[j];
+                    }
+                    currentBarang.name[currentWord.Length] = '\0';
+
+                    InsertLastArrDin(arrayItems, currentBarang);
+                }
             }
 
             // Membaca total user yang tersimpan dalam file
             ADVFileWordNewLine();
-            total = wordToInt(currentWord);
 
-            // Memasukkan informasi user ke arrayUsers
-            for (int i = 0; i < total; i++) {
-                currentUser.riwayat_pembelian = malloc(sizeof(Stack));
-                currentUser.wishlist = malloc(sizeof(List));
-                currentUser.keranjang = malloc(sizeof(Map));
-
-                CreateEmptyStack(currentUser.riwayat_pembelian);
-                CreateEmptyListLinier(currentUser.wishlist);
-                CreateEmptyMap(currentUser.keranjang);
-
-                ADVFileWordSpace();
-                currentUser.money = wordToInt(currentWord);
-
-                ADVFileWordSpace();
-                for (int j = 0; j < currentWord.Length; j++) {
-                    currentUser.name[j] = currentWord.TabWord[j];
+            for(int i = 0; i < currentWord.Length; i++){
+                if(currentWord.TabWord[i] < 48 || currentWord.TabWord[i] > 57){
+                    isIntUser = false;
                 }
-                currentUser.name[currentWord.Length] = '\0';
+            }
 
-                ADVFileWordNewLine();
-                for (int j = 0; j < currentWord.Length; j++) {
-                    currentUser.password[j] = currentWord.TabWord[j];
-                }
-                currentUser.password[currentWord.Length] = '\0';
+            if(!isIntBarang){
+                isIntUser = false;
+            }
 
-                ADVFileWordNewLine();
-                n_riwayat_pembelian = wordToInt(currentWord);
-                for (int i = 0; i < n_riwayat_pembelian; i++) {
-                    barang_dibeli X;
+            if(isIntUser){
+                total = wordToInt(currentWord);
+
+                // Memasukkan informasi user ke arrayUsers
+                for (int i = 0; i < total; i++) {
+                    currentUser.riwayat_pembelian = malloc(sizeof(Stack));
+                    currentUser.wishlist = malloc(sizeof(List));
+                    currentUser.keranjang = malloc(sizeof(Map));
+
+                    CreateEmptyStack(currentUser.riwayat_pembelian);
+                    CreateEmptyListLinier(currentUser.wishlist);
+                    CreateEmptyMap(currentUser.keranjang);
 
                     ADVFileWordSpace();
-                    X.totalharga = wordToInt(currentWord);
+                    currentUser.money = wordToInt(currentWord);
+
+                    ADVFileWordSpace();
+                    for (int j = 0; j < currentWord.Length; j++) {
+                        currentUser.name[j] = currentWord.TabWord[j];
+                    }
+                    currentUser.name[currentWord.Length] = '\0';
 
                     ADVFileWordNewLine();
                     for (int j = 0; j < currentWord.Length; j++) {
-                        X.name[j] = currentWord.TabWord[j];
+                        currentUser.password[j] = currentWord.TabWord[j];
                     }
-                    X.name[currentWord.Length] = '\0';
-                    PushStack(currentUser.riwayat_pembelian, X);
-                }
-
-                ADVFileWordNewLine();
-                n_wishlist = wordToInt(currentWord);
-                for (int i = 0; i < n_wishlist; i++) {
-                    nama_barang X;
+                    currentUser.password[currentWord.Length] = '\0';
 
                     ADVFileWordNewLine();
-                    for (int j = 0; j < currentWord.Length; j++) {
-                        X[j] = currentWord.TabWord[j];
+                    for(int i = 0; i < currentWord.Length; i++){
+                        if(currentWord.TabWord[i] == ' '){
+                            riwayat_exist = false;
+                            break;
+                        }
                     }
-                    X[currentWord.Length] = '\0';
-                    InsVLast(currentUser.wishlist, X);
-                }
 
-                SetEl(arrayUsers, i, currentUser);
+                    if(!isIntBarang || !isIntUser){
+                        riwayat_exist = false;
+                    }
+
+                    if(riwayat_exist){
+                        n_riwayat_pembelian = wordToInt(currentWord);
+                        for (int i = 0; i < n_riwayat_pembelian; i++) {
+                            barang_dibeli X;
+
+                            ADVFileWordSpace();
+                            X.totalharga = wordToInt(currentWord);
+
+                            ADVFileWordNewLine();
+                            for (int j = 0; j < currentWord.Length; j++) {
+                                X.name[j] = currentWord.TabWord[j];
+                            }
+                            X.name[currentWord.Length] = '\0';
+                            PushStack(currentUser.riwayat_pembelian, X);
+                        }
+                    }
+
+                    ADVFileWordNewLine();
+                    for(int i = 0; i < currentWord.Length; i++){
+                        if(currentWord.TabWord[i] == ' '){
+                            wishlist_exist = false;
+                            break;
+                        }
+                    }
+
+                    if(!isIntBarang || !isIntUser){
+                        wishlist_exist = false;
+                    }
+
+                    if(wishlist_exist){
+                        n_wishlist = wordToInt(currentWord);
+                        for (int i = 0; i < n_wishlist; i++) {
+                            nama_barang X;
+
+                            ADVFileWordNewLine();
+                            for (int j = 0; j < currentWord.Length; j++) {
+                                X[j] = currentWord.TabWord[j];
+                            }
+                            X[currentWord.Length] = '\0';
+                            InsVLast(currentUser.wishlist, X);
+                        }
+                    }
+                    SetEl(arrayUsers, i, currentUser);
+                }
+                arrayUsers->Neff = total;
             }
-            arrayUsers->Neff = total;
         }
 
-        if (Length(*arrayItems) == 0 || NbElmt(*arrayUsers) == 0) {
+        if (Length(*arrayItems) == 0 || NbElmt(*arrayUsers) == 0 || n_riwayat_pembelian == -1 || n_wishlist == -1) {
             printf("File tidak memuat data yang sesuai. PURRMART gagal dijalankan.\n");
         }
         else {
